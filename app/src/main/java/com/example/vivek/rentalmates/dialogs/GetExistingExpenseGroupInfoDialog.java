8204645a4f5d29ce231fbf70c247183e84a9db9c
Vivek Sharma <vivekmsit@gmail.com>
2015-11-01 21:41:59 +0530

    Added input check inside dialogfragments.

--- app/src/main/java/com/example/vivek/rentalmates/dialogs/GetExistingExpenseGroupInfoDialog.java
+++ app/src/main/java/com/example/vivek/rentalmates/dialogs/GetExistingExpenseGroupInfoDialog.java
@@ -2,6 +2,7 @@ package com.example.vivek.rentalmates.dialogs;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,14 +10,15 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.vivek.rentalmates.R;
 
 public class GetExistingExpenseGroupInfoDialog extends DialogFragment {
     private EditText expenseGroupNameEditText;
     private EditText ownerEmailIdEditText;
-
-    OnDialogResultListener listener;
+    private Context context;
+    private OnDialogResultListener listener;
 
     public interface OnDialogResultListener {
         void onPositiveResult(String expenseGroupName, String ownerEmailId);
@@ -31,6 +33,7 @@ public class GetExistingExpenseGroupInfoDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        context = getActivity().getApplicationContext();
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_fragment_join_existing_expense_group, null);
         expenseGroupNameEditText = (EditText) view.findViewById(R.id.expenseGroupNameEditText);
@@ -42,7 +45,9 @@ public class GetExistingExpenseGroupInfoDialog extends DialogFragment {
         alertDialogBuilder.setPositiveButton("Join Expense Group", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                listener.onPositiveResult(expenseGroupNameEditText.getText().toString(), ownerEmailIdEditText.getText().toString());
+                if (verifyInputData()) {
+                    listener.onPositiveResult(expenseGroupNameEditText.getText().toString(), ownerEmailIdEditText.getText().toString());
+                }
             }
         });
         alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -54,5 +59,17 @@ public class GetExistingExpenseGroupInfoDialog extends DialogFragment {
             }
         });
         return alertDialogBuilder.create();
+    }
+
+    public boolean verifyInputData() {
+        if (expenseGroupNameEditText.getText().toString().trim().matches("")) {
+            Toast.makeText(context, "No expense group name entered", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (ownerEmailIdEditText.getText().toString().trim().matches("")) {
+            Toast.makeText(context, "No owner email id entered", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            return true;
+        }
     }
 }
--- app/src/main/java/com/example/vivek/rentalmates/dialogs/GetExpenseGroupNameDialog.java
+++ app/src/main/java/com/example/vivek/rentalmates/dialogs/GetExpenseGroupNameDialog.java
@@ -2,6 +2,7 @@ package com.example.vivek.rentalmates.dialogs;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,12 +10,14 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.vivek.rentalmates.R;
 
 public class GetExpenseGroupNameDialog extends DialogFragment {
     private EditText expenseGroupNameEditText;
-    OnDialogResultListener listener;
+    private OnDialogResultListener listener;
+    private Context context;
 
     public interface OnDialogResultListener {
         void onPositiveResult(String expenseGroupName);
@@ -29,6 +32,7 @@ public class GetExpenseGroupNameDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        context = getActivity().getApplicationContext();
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_fragment_create_expense_group, null);
         expenseGroupNameEditText = (EditText) view.findViewById(R.id.expenseGroupNameEditText);
@@ -39,7 +43,9 @@ public class GetExpenseGroupNameDialog extends DialogFragment {
         alertDialogBuilder.setPositiveButton("Create Expense Group", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                listener.onPositiveResult(expenseGroupNameEditText.getText().toString());
+                if (verifyInputData()) {
+                    listener.onPositiveResult(expenseGroupNameEditText.getText().toString());
+                }
             }
         });
         alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -51,5 +57,14 @@ public class GetExpenseGroupNameDialog extends DialogFragment {
             }
         });
         return alertDialogBuilder.create();
+    }
+
+    public boolean verifyInputData() {
+        if (expenseGroupNameEditText.getText().toString().trim().matches("")) {
+            Toast.makeText(context, "No expense group name entered", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            return true;
+        }
     }
 }
